361c593bbed0d2acad1ec3260cfa6790231d1d88
kamille-rabelo <kamillersalgado@gmail.com>
2024-08-19 15:23:50 -0300

    add create and viewing function

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+let data = [];
+
+function addData(title, content) {
+    data.push({Title: title, Content: content.replace(/\r\n/g, '<br>')});
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {data: data});
+});
+
+app.post("/", (req, res) => {
+    addData(req.body["postTitle"], req.body["postContent"]);
+    console.log(data);
+    res.redirect("/")
 });
 
 app.listen(port, () => {
