0ddff1b1739e7ebce51d2ce7e777576d53a562c1
kamille-rabelo <kamillersalgado@gmail.com>
2024-08-20 11:01:58 -0300

    add delete function

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ const port = 3000;
 
 let data = [];
 
-function addData(title, content, index="") {
+function addData(title, content, index="", titleChange="") {
     let today = new Date();
-    today = today.getMonth() + '/' + today.getDate() + '/' +today.getFullYear()
-    index? data[index] = {title: title, content: content.replace(/\r\n/g, '<br>'), date: today} : data.push({title: title, content: content.replace(/\r\n/g, '<br>'), date: today});
+    today = today.getMonth() + '/' + today.getDate() + '/' + today.getFullYear();
+    if (index) {
+        if (data[index].title === titleChange) {data[index] = {title: title, content: content.replace(/\r\n/g, '<br>'), date: today}};
+    } else {
+        data.push({title: title, content: content.replace(/\r\n/g, '<br>'), date: today});
+    }
+}
+
+function deleteData(index, title) {
+    if (data.includes(data[index])) {
+        if (data[index].title === title) {
+            data.splice(index, 1);
+            return true;
+        };
+    };
+    console.log("Invalid value for delete function.");
+    return false;
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +36,11 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    req.body["inputChange"]? addData(req.body["postTitle"], req.body["postContent"], req.body["inputChange"]) : addData(req.body["postTitle"], req.body["postContent"]);
+    if (req.body["postIndex"]) {
+        deleteData(req.body["postIndex"], req.body["postTitle"]);
+    } else {
+        req.body["inputChange"]? addData(req.body["postTitle"], req.body["postContent"], req.body["inputChange"], req.body["titleChange"]) : addData(req.body["postTitle"], req.body["postContent"]);
+    }
     res.redirect("/")
 });
 
